2d46fe402350c6207613604a1ccd154598eee947
Shelby Goff <shelby.goff@gmail.com>
2019-08-05 22:04:30 -0700

    Add files

---
+++ Lesson02/calculator.js
@@ -0,0 +1,14 @@
+const x = 4;
+const y = 3;
+const z = 5;
+const a = 10;
+let b = 'number ';
+
+
+console.log(x+y);
+console.log(x**y);
+console.log((x+y)*a);
+console.log((a-z)+z**y);
+console.log(b+a);
+console.log((a-x)-z));
+console.log(b+(x*y));
