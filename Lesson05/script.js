cad834fd91aac74c5da2bc9ace0d3fe945edaeb3
Shelby Goff <shelby.goff@gmail.com>
2019-08-27 15:57:25 -0700

    add files

--- Lesson05/script.js
+++ Lesson05/script.js
@@ -5,13 +5,14 @@ let loop = true;
 function BalancePrompt() {
   if (balance < 300) alert('Warning: Your balance is less than $300.');
   loop = true;
+  }
   while (loop) {
     let input = prompt('What would you like to do? B: View your Balance, D: Make a Deposit, W: Make a Withdrawal, Q: Close MeBank App');
 
   if (input === 'Q') {
     loop = false;
     break;
-  }
+    }
   else if (input === 'W') {
     makeWithdrawl();
     }
@@ -20,14 +21,14 @@ function BalancePrompt() {
     }
   else if (input === 'B') {
     showBalance();
-  }
+    }
   else {
     alert('I don\'t understand that command. Please try again.');
-  }
+    }
+}
 
 function showBalance() {
   alert('Your balance is currently $' + (balance));
-}
 }
 
 function makeWithdrawl() {
@@ -39,13 +40,12 @@ function makeWithdrawl() {
 }
 
 function makeDeposit() {
-  let depositAmount = Number(prompt("How much would you like to deposit? "));
+  let depositAmount = Number(prompt("How much would you like to deposit?"));
   let newBalance = (balance + depositAmount);
 
   balance = newBalance;
   showBalance();
   }
-}
 
 //quit the app
 function handlerQuit() {
